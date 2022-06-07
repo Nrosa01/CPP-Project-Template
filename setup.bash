7cbcee50590b4d475bc405fb41f774d125111378
Nicolas Rosa Caballero <nrosa01@ucm.es>
2022-06-07 12:51:57 +0200

    Git folder wan't deleting correctly

--- setup.bash
+++ setup.bash
@@ -3,4 +3,4 @@ git clone https://github.com/Nrosa01/CPP-Project-Template
 rm ./CPP-Project-Template/LICENSE
 rm ./CPP-Project-Template/README.md
 rm ./CPP-Project-Template/setup.bash
-rm -r ./CPP-Project-Template/git
+rm -r ./CPP-Project-Template/".git"
