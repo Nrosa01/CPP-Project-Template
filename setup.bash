94e1488c10e638f9139f71ded1204d5307fbad82
Nicolas Rosa Caballero <nrosa01@ucm.es>
2022-06-07 12:42:29 +0200

    Setup bat fixes

--- setup.bash
+++ setup.bash
@@ -1,7 +1,6 @@
-REMOTE_URL="https://github.com/Nrosa01/CPP-Project-Template"
-
-git clone $REMOTE_URL
+git clone https://github.com/Nrosa01/CPP-Project-Template
 
 rm ./CPP-Project-Template/LICENSE
 rm ./CPP-Project-Template/README.md
 rm ./CPP-Project-Template/setup.bash
+rm -r ./CPP-Project-Template/git
