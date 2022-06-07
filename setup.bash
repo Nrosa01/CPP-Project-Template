5c9697811880a63a33f0c14321ce65cd763f0762
Nicolas Rosa Caballero <nrosa01@ucm.es>
2022-06-07 12:55:39 +0200

    Now all flies are in root dir when running setup bash

--- setup.bash
+++ setup.bash
@@ -3,4 +3,7 @@ git clone https://github.com/Nrosa01/CPP-Project-Template
 rm ./CPP-Project-Template/LICENSE
 rm ./CPP-Project-Template/README.md
 rm ./CPP-Project-Template/setup.bash
-rm -r ./CPP-Project-Template/".git"
+rm -rf ./CPP-Project-Template/.git
+
+mv ./CPP-Project-Template/* ./
+
