57b6acfdbc5d1f59bc9cb4c9e72624e818e77998
Nicolas Rosa Caballero <nrosa01@ucm.es>
2022-06-07 13:55:21 +0200

    setup cleaning

--- setupCPP.bash
+++ setupCPP.bash
@@ -3,6 +3,7 @@ git clone https://github.com/Nrosa01/CPP-Project-Template
 rm ./CPP-Project-Template/LICENSE
 rm ./CPP-Project-Template/README.md
 rm ./CPP-Project-Template/setupCPP.bash
+rm ./CPP-Project-Template/setupC.bash
 cd ./CPP-Project-Template
 rm -rf .git
 
