cd3d20c5c885b520d89c137e74e64136c99e62ee
Nicolas Rosa Caballero <nrosa01@ucm.es>
2022-06-07 13:00:30 +0200

    Or now

--- setup.bash
+++ setup.bash
@@ -5,7 +5,6 @@ rm ./CPP-Project-Template/README.md
 rm ./CPP-Project-Template/setup.bash
 cd ./CPP-Project-Template
 rm -rf .git
-cd ..
 
-mv ./CPP-Project-Template/* ./
-rm -rf ./CPP-Project-Template
+mv ./C++-Project-Template/* ./
+cd ..
