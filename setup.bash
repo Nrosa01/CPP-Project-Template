7cddd6ad0417ce265b2e80616f13b0a76efd205e
Nicolas Rosa Caballero <nrosa01@ucm.es>
2022-06-07 12:57:41 +0200

    I don't know bash

--- setup.bash
+++ setup.bash
@@ -3,7 +3,9 @@ git clone https://github.com/Nrosa01/CPP-Project-Template
 rm ./CPP-Project-Template/LICENSE
 rm ./CPP-Project-Template/README.md
 rm ./CPP-Project-Template/setup.bash
-rm -rf ./CPP-Project-Template/.git
+cd ./CPP-Project-Template
+rm -rf .git
+cd ..
 
 mv ./CPP-Project-Template/* ./
 
